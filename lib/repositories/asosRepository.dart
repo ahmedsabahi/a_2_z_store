712ec227a1365aa089244320e55d90d255e6f583
Ahmed Sabahi <a7md2shrf@gmail.com>
2021-04-14 01:19:17 +0200

    add search A2Z category screen men and women

--- lib/repositories/asosRepository.dart
+++ lib/repositories/asosRepository.dart
@@ -1,9 +1,10 @@
 import 'package:a_2_z_store/models/categoriesList.dart';
 import 'package:a_2_z_store/models/productDetails.dart';
 import 'package:a_2_z_store/models/productList.dart';
+import 'package:a_2_z_store/services/asosService.dart';
 
 class AsosRepository {
-  AsosRepository _asosRepository = AsosRepository();
+  AsosServices _asosRepository = AsosServices();
 
   Future<CategoriesList> fetchCategoriesList() {
     return _asosRepository.fetchCategoriesList();
--- lib/widgets/brandsCategory/brandsCategoryProvider.dart
+++ lib/widgets/brandsCategory/brandsCategoryProvider.dart
@@ -2,12 +2,12 @@ import 'package:a_2_z_store/models/categoriesList.dart';
 import 'package:a_2_z_store/repositories/asosRepository.dart';
 import 'package:flutter/material.dart';
 
-class SearchScreenProvider extends ChangeNotifier {
+class BrandsCategoryProvider extends ChangeNotifier {
   CategoriesList list;
 
   AsosRepository _asosRepository = AsosRepository();
 
-  SearchScreenProvider() {
+  BrandsCategoryProvider() {
     getCategories();
   }
 
